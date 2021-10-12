c82854825449490ff26bc0902edfccc61a097249
nushohel <nushohel2@gmail.com>
2021-10-12 11:45:47 +0600

    fetched data & setup UI

--- src/component/Filter.tsx
+++ src/component/Filter.tsx
@@ -9,21 +9,16 @@ const Filter = () => {
         <option value='volvo'>Volvo</option>
         <option value='saab'>Saab</option>
         <option value='opel'>Opel</option>
-        <option value='audi'>Audi</option>
       </select>
       <label htmlFor='cars'>Choose a car:</label>
       <select name='cars' id='cars'>
         <option value='volvo'>Volvo</option>
         <option value='saab'>Saab</option>
-        <option value='opel'>Opel</option>
-        <option value='audi'>Audi</option>
       </select>
       <label htmlFor='cars'>Choose a car:</label>
       <select name='cars' id='cars'>
         <option value='volvo'>Volvo</option>
         <option value='saab'>Saab</option>
-        <option value='opel'>Opel</option>
-        <option value='audi'>Audi</option>
       </select>
     </div>
   );
