599d9f3dba719b62cac9096be3a82ad12c364f84
nushohel <nushohel2@gmail.com>
2021-10-12 20:41:50 +0600

    added redux createSlice and connected to app

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,36 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
 import { Container } from "reactstrap";
 import "./App.css";
 import Filter from "./component/Filter";
 import NavBar from "./component/NavBar";
 import SpaceCrapt from "./component/SpaceCrapt";
+import { incrementByAmount,filter } from "./store/counterSlice";
+import { RootState } from "./store/store";
+
 
 const App: React.FC = () => {
-  const [state, setState] = useState<[]>([]);
+  const count = useSelector((state: RootState) => state.counter.value);
+  const dispatch = useDispatch();
   useEffect(() => {
     fetch("https://api.spacexdata.com/v3/launches")
       .then((res) => res.json())
-      .then((data) => setState(data));
-  }, []);
-  console.log(state);
+      .then((data) => {
+      dispatch(filter("Success"))
+      });
+  }, [dispatch]);
+  console.log(count);
 
   return (
     <div className='app'>
-    <Container>
-      <NavBar />
-      <Filter/>
-      <div className='d-flex flex-wrap justify-content-between'>
-        {state.map((rocket,index) => (
-          <SpaceCrapt rocket={rocket} key={index} />
-        ))}
-      </div>
+      <Container>
+        <NavBar />
+        <Filter />
+        <div className='d-flex flex-wrap justify-content-between'>
+          {count.map((rocket, index) => (
+            <SpaceCrapt rocket={rocket} key={index} />
+          ))}
+        </div>
       </Container>
     </div>
   );
